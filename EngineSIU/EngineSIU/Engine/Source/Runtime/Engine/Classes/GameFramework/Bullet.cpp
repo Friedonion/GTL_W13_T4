d50fa72e4c6e9aa0d5456943386176e38b253e1b
Jay <seojoongwon2020@gmail.com>
2025-05-31 23:39:38 +0900

    Add: Bullet.cpp 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/Bullet.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/Bullet.cpp
@@ -1,7 +1,18 @@
 #include "Bullet.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/ProjectileMovementComponent.h"
 
 ABullet::ABullet()
 {
+    // Tick 을 사용할 수 있게 허용 (이 액터가 Tick() 을 호출받도록)
+    //PrimaryActorTick.bCanEverTick = true;
+
+    // 메시 컴포넌트 생성 -> BulletMesh 에 저장 -> 루트 컴포넌트로 설정
+    BulletMesh = AddComponent<UStaticMeshComponent>(TEXT("BulletMesh"));
+    SetRootComponent(BulletMesh);
+
+    // 이동 컴포넌트를 생성 -> ProjectileMovement 에 저장
+    ProjectileMovement = AddComponent<UProjectileMovementComponent>(TEXT("BulletMovement"));
 }
 
 ABullet::~ABullet()
@@ -10,12 +21,19 @@ ABullet::~ABullet()
 
 void ABullet::BeginPlay()
 {
+    Super::BeginPlay();
 }
 
 void ABullet::Tick(float DeltaTime)
 {
+    Super::Tick(DeltaTime);
 }
 
-void ABullet::FireInDirection(const FVector& ShootDirection)
+void ABullet::FireInDirection(const FVector& InitialVelocity)
 {
+    if (ProjectileMovement)
+    {
+        // ProjectileMovement 컴포넌트가 제공하는 SetVelocity 메서드를 호출
+        ProjectileMovement->SetVelocity(InitialVelocity);
+    }
 }
