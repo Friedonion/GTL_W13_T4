68ec00ad735a9d593ea5e50ca99521fb51f8c819
FriedOnion <rkdwnd333@naver.com>
2025-06-02 20:16:24 +0900

    wip : enemy scale 조절

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/SocketComponent.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/SocketComponent.cpp
@@ -22,6 +22,7 @@ void USocketComponent::TickComponent(float DeltaTime)
         const FTransform SocketTransform = SkeletalMeshComponent->GetSocketTransform(Socket);
         SetRelativeRotation(SocketTransform.GetRotation().Rotator());
         SetRelativeLocation(SocketTransform.GetTranslation());
+        SetRelativeScale3D(SocketTransform.GetScale3D());
     }
 }
 
