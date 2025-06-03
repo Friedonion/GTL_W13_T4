4747dc9c8cdc2d731c2da2c76e59258d5a1a353c
FriedOnion <rkdwnd333@naver.com>
2025-06-03 16:13:12 +0900

    feat: 기본 UI 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/UncannyGameMode.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "GameMode.h"
+
+class AUncannyGameMode : public AGameMode
+{
+    DECLARE_CLASS(AUncannyGameMode, AGameMode)
+
+public:
+    AUncannyGameMode() = default;
+    virtual ~AUncannyGameMode() override = default;
+
+    virtual void PostSpawnInitialize() override;
+    virtual void Tick(float DeltaTime) override;
+
+    void SetCurrentHP(int32 NewHP);
+    void SetMaxHP(int32 NewMaxHP);
+    void SetBulletCount(int32 NewCount);
+
+protected:
+    void UpdateUI();
+
+private:
+    int32 CurrentHP = 100;
+    int32 MaxHP = 100;
+    int32 BulletCount = 30;
+    int32 KillCount = 0;
+
+    FString KillCountName = "UI_KillCount";
+    FString HPTextName = "UI_HPText";
+    FString BulletTextName = "UI_BulletText";
+};
